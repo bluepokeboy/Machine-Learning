b9b9fb0c30f2972aec99762656c89c65d29b37e5
bluepokeboy <42347117+bluepokeboy@users.noreply.github.com>
2019-06-21 12:59:38 +0530

    Update simplelinearregression.py

--- simplelinearregression.py
+++ simplelinearregression.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib as plt
 def linearregression(x,y):
     b1=0
     b2=0
@@ -21,6 +22,23 @@ def linearregression(x,y):
         b2new=b2-a*d2
     return(b1,b2)
 
+def plot_regression_line(x, y, b): 
+    # plotting the actual points as scatter plot 
+    plt.scatter(x, y, color = "m", 
+               marker = "o", s = 30) 
+  
+    # predicted response vector 
+    y_pred = b[0] + b[1]*x 
+  
+    # plotting the regression line 
+    plt.plot(x, y_pred, color = "g") 
+  
+    # putting labels 
+    plt.xlabel('x') 
+    plt.ylabel('y') 
+  
+    # function to show plot 
+    plt.show() 
 
 def main():
     x = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
@@ -30,7 +48,8 @@ def main():
     # a=0.25
     # print(np.sum(np.multiply((b1*x+a),x)))
     answer=linearregression(x,y)
-    print(answer)
+    #print(answer)
+    plot_regression_line(x,y,answer)
 
 if __name__== "__main__":
     main()
